e0d5ca63fe872f1a0fc90128b9918626b36b630e
matias <matisanfi@gmail.com>
2018-01-29 10:37:50 -0300

    ko2

--- ngrxchannel/src/app/app.module.ts
+++ ngrxchannel/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { ChannelviewComponent } from './components/channelview/channelview.component';
+import { ChannelselectorComponent } from './components/channelselector/channelselector.component';
+import {uiState} from "./store/reducers/uiStateReducer";
+import {storeData} from "./store/reducers/uiStoreDataReducer";
+import {INITIAL_APPLICATION_STATE} from "./store/application-state";
+import {StoreModule} from "@ngrx/store";
+import {EffectsModule} from "@ngrx/effects";
+import {LoadChannelsEffectService} from "./store/effects/load-channels-effect-service";
+import {ChannelService} from "./services/channel.service";
+import {HttpModule} from "@angular/http";
+import { ChannelListComponent } from './components/channel-list/channel-list.component';
+import { ChannelMessagesComponent } from './components/channel-messages/channel-messages.component';
+
+
+const reducers = {
+  uiState,
+  storeData
+};
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChannelviewComponent
+    ChannelviewComponent,
+    ChannelselectorComponent,
+    ChannelListComponent,
+    ChannelMessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    StoreModule.forRoot(reducers, {initialState: INITIAL_APPLICATION_STATE}),
+    EffectsModule.forRoot([LoadChannelsEffectService])
   ],
-  providers: [],
+  providers: [ChannelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ngrxchannel/src/main.ts
+++ ngrxchannel/src/main.ts
@@ -4,6 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+
+import "rxjs/add/observable/interval";
+import "rxjs/add/observable/of";
+
 if (environment.production) {
   enableProdMode();
 }
